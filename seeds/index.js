cc8e2568a5a4c94d51a3a24f6205c5382de65409
WhiteArro <damitrafi6@gmail.com>
2021-01-21 15:36:15 +0100

    campgrounds displayed from younger to older

--- seeds/index.js
+++ seeds/index.js
@@ -3,6 +3,7 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
+
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
    useNewUrlParser: true,
    useCreateIndex: true,
